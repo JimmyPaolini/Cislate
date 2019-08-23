64f977cd3db519b9899192a2251d7dc7b36529d6
Jimmy Paolini <jimmypaolini@gmail.com>
2019-08-23 13:40:17 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+import cherrypy, requests, re, webbrowser, os, os.path
+from bs4 import BeautifulSoup
+
+directory = os.path.join(os.path.dirname(os.path.realpath(__file__)))
+
+class Cislate(): # http://127.0.0.1:3800
+    @cherrypy.expose
+    def index(self):
+        return open(directory + '/static/index.html')
+
+    @cherrypy.expose
+    def shutdown(self):
+        print("SHUTDOWN")
+        cherrypy.process.wspbus.Bus.exit()
+        #subprocess.call(['lsof -ti tcp:3800 | xargs kill'])
+
+    @cherrypy.expose
+    def translate(self, latin):
+        if cherrypy.engine.state != cherrypy.engine.states.STARTED:
+            return "server shutdown, restart to translate"
+        url = 'http://archives.nd.edu/cgi-bin/wordz.pl?keyword='
+        request = requests.get(url + latin)
+        html = BeautifulSoup(request.text, 'html.parser')
+        translation = latin + "\n\n" + str(html.pre)[6:-9]
+        return translation
+
+    @cherrypy.expose
+    def processLatin(self, latin):
+        if cherrypy.engine.state != cherrypy.engine.states.STARTED:
+            return "server shutdown, restart to translate"
+        return ''.join(['<span>'+elt+'</span>\n' if elt is not '\n' else '<br>' for elt in re.findall(r'\S+|\n',latin)])
+
+if __name__ == "__main__":
+    webbrowser.open_new_tab("http://127.0.0.1:3800")
+    cherrypy.quickstart(Cislate(), '/', {
+        'global' : {
+            'server.socket_host' : '127.0.0.1',
+            'server.socket_port' : 3800,
+            'server.shutdown_timeout': 1
+        },
+        '/': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': directory
+        },
+        '/style.css': {
+            'tools.staticfile.on': True,
+            'tools.staticfile.filename': os.path.abspath('./style.css')
+        },
+        '/favicon.ico': {
+            'tools.staticfile.on': True,
+            'tools.staticfile.filename': os.path.abspath('./favicon.ico')
+        }
+    })
