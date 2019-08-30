e06a3ac7e0dc549db2ad9f3f7330a622105a4eb6
Jimmy Paolini <jimmypaolini@gmail.com>
2019-08-30 17:30:28 -0400

    Format and Routing fixes

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import cherrypy, requests, re, webbrowser, os, os.path
 from bs4 import BeautifulSoup
 
+root = os.path.abspath('')
+
 class Cislate(): # http://127.0.0.1:3800
     @cherrypy.expose
     def index(self):
-        return open(os.path.join(os.path.abspath(''), 'static/index.html'))
+        return open(os.path.join(root, 'static/index.html'))
 
     @cherrypy.expose
     def shutdown(self):
@@ -38,18 +40,18 @@ if __name__ == "__main__":
         },
         '/': {
             'tools.staticdir.on': True,
-            'tools.staticdir.dir': os.path.abspath('')
+            'tools.staticdir.dir': root
         },
         '/static': {
             'tools.staticfile.on': True,
-            'tools.staticfile.filename': os.path.abspath('./static')
+            'tools.staticfile.filename': os.path.join(root, 'static')
         },
         '/style.css': {
             'tools.staticfile.on': True,
-            'tools.staticfile.filename': os.path.abspath('./static/style.css')
+            'tools.staticfile.filename': os.path.join(root, 'static/style.css')
         },
         '/favicon.ico': {
             'tools.staticfile.on': True,
-            'tools.staticfile.filename': os.path.abspath('./static/favicon.ico')
+            'tools.staticfile.filename': os.path.join(root, 'static/favicon.ico')
         }
     })
