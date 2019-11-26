2d9394efc3d806254fddf13b086e47d1fbabc703
Jimmy Paolini <jimmypaolini@gmail.com>
2019-11-26 14:49:43 -0500

    Remove tests

--- test.js
+++
@@ -1,7 +0,0 @@
-const request = require("request-promise-native");
-
-(async () => {
-  const url = 'https://nqhfwgv4ib.execute-api.us-east-2.amazonaws.com/default/cislate?latin=' + 'luna';
-  const response = await request.get(url, {headers: {'x-api-key': 'zDy6OMHcqi89AYF4J0kXO8bbxOzCUXb5afeHAkNs'}});
-  console.log(response);
-})()
